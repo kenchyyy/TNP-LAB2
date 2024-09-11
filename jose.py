ff8ceed9d7cf2f05543fe55fee510f3812765d1d
Kenneth Joel Urbano <kenneth.urbano0127@gmail.com>
2024-09-11 13:45:35 +0800

    first commit

---
+++ jose.py
@@ -0,0 +1,12 @@
+def carol():
+    print("Whenever I see girls and boys..")
+
+for i in range(4):
+    carol()
+    print("MERRY CHRISTMAS!")
+
+
+class Tree:
+
+    def __init__(self, name):
+        self.name = name
