fca856fa2906a8502242574b305ce4b3d1bd93c6
asilyN <150698908+asilyN@users.noreply.github.com.>
2024-09-11 14:26:39 +0800

    feature ini

--- marie.py
+++ marie.py
@@ -1,5 +1,6 @@
 def sing_a_song():
     print("We wish you a Merry Christmas and a Happy New Year")
     print("Thank youuuuuuuu, Thank youuuuuuu, ang babait ninyo THANK YOU!!!")
+    print("Mamamo nyo")
 
 sing_a_song()
