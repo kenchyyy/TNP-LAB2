5755207ea62796c23200bc85aa76eea72407c3a3
asilyN <150698908+asilyN@users.noreply.github.com.>
2024-09-11 14:28:20 +0800

    fast forward ni

--- marie.py
+++ marie.py
@@ -2,5 +2,6 @@ def sing_a_song():
     print("We wish you a Merry Christmas and a Happy New Year")
     print("Thank youuuuuuuu, Thank youuuuuuu, ang babait ninyo THANK YOU!!!")
     print("Mamamo nyo")
+    print("Mamamo mo")
 
 sing_a_song()
